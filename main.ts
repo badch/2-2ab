aead06427ec545f22dafdd2850c4f111d5e69577
badch <50537232+badch@users.noreply.github.com>
2021-09-01 22:45:11 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    images.createBigImage(`
+        # . . . # # . . . #
+        . # . # . . # . # .
+        . . # . . . . # . .
+        . # . # . . # . # .
+        # . . . # # . . . #
+        `).scrollImage(0.1, 200)
+})
